40a7c23f3caa0511acb48063c0a86cca4c6b3fa1
salimM-code <mustafasalim000@gmail.com>
2021-12-13 13:02:12 -0500

    bean count solution

---
+++ 03_Functions/beanCounting.js
@@ -0,0 +1,21 @@
+const countBs = (str) => {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if(str[i] === 'B'){
+      count += 1;
+    }
+  }
+  return count;
+}
+
+const charCount = (str, char) => {
+  let count = 0;
+  for(let i = 0; i < str.length; i++){
+    if(str[i] === char){
+      count += 1;
+    }
+  }
+  return count;
+}
+console.log(countBs("HelloBuddyB"))
